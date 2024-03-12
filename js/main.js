dc61973b2a2334a52e2210b12998c0c51afdade6
nakatani022 <ame@DESKTOP-LKL4NOI.localdomain>
2024-03-12 22:31:09 +0900

    programタブの配列変更

--- js/main.js
+++ js/main.js
@@ -1,57 +1,152 @@
 "use strict";
 
-// // スクロールヘッダー固定
-// $(function(){
-//   let navPos =$(".news").offset().top;
+// ハンバーガーメニューの挙動
+const header_sp = document.querySelector(".headersp");
+const header = document.querySelector(".header");
+const sp_nav = document.querySelector(".sp_nav");
+const sp_menu = document.querySelector(".sp_menu");
 
-//   $(window).scroll(function(){
-//     if($(window).scrollTop() > navPos && !$(".header").hasClass('open_menu')){
-//       $(".header").addClass('active');
-//       $(".header.active").slideDown(600);
-//       $(".header_link").addClass('active')
-//       $(".header_img").css('display','none');
-//       $(".header_img_bl").css('display','block');
-//       $(".header_humburger").children().css('background','#000');
-//     }else{
-//       $(".header").removeClass('active');
-//       $(".header_link").removeClass('active');
-//       $(".header_img_bl").css('display','none');
-//       $(".header_img").css('display','block');
-//       $(".header_humburger").children().css('background','#fff');
-//     }
-//   });
-// });
-
-// ハンバーガーメニュー
-// sp-header
-{
-  $(function () {
-    $(".headersp").click(function () {
-      $(this).toggleClass("close");
-      $(".header").toggleClass("open_menu");
-      $(".sp_nav").toggleClass("open_menu");
-      $(".sp_menu").toggleClass("open_menu");
-      $(".header_humburger").children().css("background", "#fff");
-      // $(".header_img_bl").css('display','none');
-      // $(".header_img").css('display','block');
-    });
-  });
-}
-{
-  $(function () {
-    $(".sp_menu a").click(function () {
-      $(".sp_menu").removeClass("open_menu");
-      // $(".header").removeClass("open_menu");
-      $(".header").removeClass("open_menu");
-      $(".header_sp").removeClass("close");
-    });
-  });
+const toggle_menu = ()=> {
+  header_sp.classList.toggle('close');
+  header.classList.toggle('open_menu');
+  sp_nav.classList.toggle('open_menu');
+  sp_menu.classList.toggle('open_menu');
 }
 
-// ローディングアニメーション
-{
-  window.onload = function () {
-    const spinner = document.getElementById("loading_animation");
-    spinner.classList.add("loaded");
+header_sp.addEventListener('click', toggle_menu);
+
+// タブリスト
+const tab_all = document.querySelector("#tab_all");
+const tab_program = document.querySelector("#tab_program");
+const tab_design = document.querySelector("#tab_design");
+const check = document.querySelectorAll(".check");
+
+
+
+
+
+
+
+
+// タブの切り替え
+const tab_lists = document.querySelectorAll('.tab_item');
+
+tab_lists[0].classList.add('check');
+
+
+
+
+
+
+const work_box = document.querySelector("#work_box");
+
+const work_cards = [
+  {
+    info: '一般社団法人MBTI連盟',
+    title: 'MBTI',
+    label: 'Owned Media',
+    url: './work_list/mbti.html',
+    src: './img/mbti_thumbnail.png',
+    type: 'tab_design'
+  },
+  {
+    info: '株式会社サラベル',
+    title: 'Sarahbel Inc.',
+    label: 'コーポレートサイト',
+    url: './work_list/sarahbelInc.html',
+    src: './img/sarahbel_thumbnail3.png',
+    type: 'tab_design'
+  },
+  {
+    info: 'キャンプサイト',
+    title: 'GRAM CAMP SQUARE',
+    label: 'コーポレートサイト',
+    url: './work_list/gramcampsquare.html',
+    src: './img/gram_camp_square_thumbnail.png',
+    type: 'tab_design'
+  },
+  {
+    info: 'ガーデンウエディング',
+    title: 'Lumiere Naturel',
+    label: 'LP',
+    url: './work_list/gardenwedding.html',
+    src: './img/garden_wedding_thumbnail.png',
+    type: 'tab_design'
+  },
+  {
+    info: 'GoogleBooksAPI×React×TypeScript',
+    title: '知識の森',
+    label: 'Webアプリ',
+    url: './work_list/gardenwedding.html',
+    src: './img/google_books_thumbnail.png',
+    type: 'tab_program'
+  },
+  {
+    info: 'cording',
+    title: 'sonozaki_corporate',
+    label: 'Webサイト',
+    url: './work_list/gardenwedding.html',
+    src: './img/sonozaki_corporate_thumbnail.png',
+    type: 'tab_program'
+  },
+  {
+    info: 'my portfolio',
+    title: 'nakatani portfolio',
+    label: 'Webサイト',
+    url: './work_list/gardenwedding.html',
+    src: './img/my_portfolio_thumbnail.png',
+    type: 'tab_program'
+  },
+
+
+]
+
+
+  for(let i=0; i < work_cards.length; i++){
+    const {info, title, label, url, src} = work_cards[i];
+    console.log(url);
+    const card_create = `<li class="work_card" >
+    <a class="work_card_inner" href="${url}"><div class="work_content"><p class="work_info">${info}</p><h3 class="work_lead">${title}</h3><label class="work_label">${label}</label><button class="work_button"><div class="work_button_text">制作実績<span class="work_icon"><img class="work_icon_button" src="./img/Button__Icon.svg" alt=""></span></div></button></div><div class="work_wrapper"><img class="work_thumbnail" src="${src}" alt="キャプチャ:${title}のサムネイル"></div></a>`;
+
+    work_box.insertAdjacentHTML('beforebegin', card_create);
+  }
+
+  // thisを使用するために関数宣言を使用する
+  // タブの切り替えボタン
+function tab_switch() {
+  document.querySelector('.check').classList.remove('check');
+  this.classList.add('check');
+
+  // プログラムタブが選択された場合
+  // 配列からtypeとタブの名前が同一ではないものを非表示にする
+  if(tab_program.classList.contains('check')){
+    for(let i=0; i < work_cards.length; i++){
+      document.querySelectorAll('.work_card')[i].style.display = 'block';
+      if(work_cards[i].type != 'tab_program'){
+        document.querySelectorAll('.work_card')[i].style.display = 'none';
+      }
+    }
+  } else if (tab_design.classList.contains('check')) {
+    for(let i=0; i < work_cards.length; i++){
+      document.querySelectorAll('.work_card')[i].style.display = 'block';
+      if(work_cards[i].type != 'tab_design'){
+        document.querySelectorAll('.work_card')[i].style.display = 'none';
+      }
+    }
+  } else {
+    for(let i=0; i < work_cards.length; i++){
+    document.querySelectorAll('.work_card')[i].style.display = 'block';
+  }
   };
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  for(let i= 0; i < tab_lists.length; i++){
+    tab_lists[i].addEventListener('click', tab_switch);
+  }
+});
+
+
+const tab_contents = document.querySelectorAll('.work_card');
+  
+tab_contents[0].classList.add('show');
